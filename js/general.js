41145b75bcbae87f1845f1acb0a1495de6e1c487
Armando <armando.vizcaino@enapsys.com>
2019-07-27 11:05:46 -0500

    text

---
+++ js/general.js
@@ -0,0 +1,67 @@
+$(".hover1").hover(function() {
+    $(".hover1").addClass("zoom");
+}, function() {
+    $(".hover1").removeClass("zoom");
+});
+
+$(".hover2").hover(function() {
+    $(".hover2").addClass("zoom");
+}, function() {
+    $(".hover2").removeClass("zoom");
+});
+
+
+$(document).ready(function() {
+    $(".section button").click(function() {
+        $(".text-modal").html(this.id + " selected");
+        $("#modalDesplegable").modal();
+    });
+});
+
+
+var slider = $('#slider');
+var siguiente = $('#btn-next');
+var anterior = $('#btn-prev');
+
+//mover ultima imagen al primer lugar
+$('#slider .slider__section:last').insertBefore('#slider .slider__section:first');
+//mostrar la primera imagen con un margen de -100%
+slider.css('margin-left', '-' + 100 + '%');
+
+function moverDerecha() {
+    slider.animate({
+        marginLeft: '-' + 200 + '%'
+    }, 700, function() {
+        $('#slider .slider__section:first').insertAfter('#slider .slider__section:last');
+        slider.css('margin-left', '-' + 100 + '%');
+    });
+}
+
+function moverIzquierda() {
+    slider.animate({
+        marginLeft: 0
+    }, 700, function() {
+        $('#slider .slider__section:last').insertBefore('#slider .slider__section:first');
+        slider.css('margin-left', '-' + 100 + '%');
+    });
+}
+//hacer que el slider sea automático
+function autoplay() {
+    interval = setInterval(function() {
+        moverDerecha();
+    }, 5000);
+}
+
+siguiente.on('click', function() {
+    moverDerecha();
+    clearInterval(interval);
+    autoplay();
+});
+
+anterior.on('click', function() {
+    moverIzquierda();
+    clearInterval(interval);
+    autoplay();
+});
+
+autoplay();
